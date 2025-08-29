ee67aa160af57cae0fea3aa9809e266b44e69121
gbiv <gerrick.bivins@vertex3d.com>
2025-08-29 10:25:34 -0500

    update deps,wrapper

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o /tmp/awscliv2.z
 unzip -q /tmp/awscliv2.zip -d /tmp/
 /tmp/aws/install
 
-mvn -U clean compile package install -Dmaven.test.skip=true -Dmaven.repo.local=./m2
+./mvnw -U clean compile package install -Dmaven.test.skip=true -Dmaven.repo.local=./m2
 aws s3 --profile ops cp m2/com/orbitz/consul/consul-client/ s3://vertex-code-ops-us-west-2/m2/release/com/orbitz/consul/consul-client/ --recursive --exclude _remote.repositories
 
 rm -rf m2
